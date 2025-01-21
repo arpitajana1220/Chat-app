bbf7ecb51401565fbf4e00de7c10e143bb881fc1
Arpita <arpitajana2000@gmail.com>
2025-01-21 18:46:26 +0530

    database connection

--- a_core/settings.py
+++ a_core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 from environ import Env
 env = Env()
@@ -117,7 +118,7 @@ DATABASES = {
     }
 }
 
-
+DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
